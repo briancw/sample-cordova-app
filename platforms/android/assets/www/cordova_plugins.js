1d57853785c981a1c0cead3eb9eda38f5c52c8e3
Brian Whicheloe <brian@Brians-MacBook-Pro.local>
2015-11-29 00:39:38 -0800

    Initial Commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/plugin.http.request/www/http-request.js",
+        "id": "plugin.http.request.phonegap-http-requst",
+        "pluginId": "plugin.http.request",
+        "clobbers": [
+            "cordova.plugins.http-request"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "phonegap-plugin-push": "1.4.4",
+    "plugin.http.request": "1.0.4"
+}
+// BOTTOM OF METADATA
+});
---
+++ www/js/index.js
@@ -0,0 +1,54 @@
+/* jshint browser: true */
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+        this.server_url = 'http://192.168.1.35:9030';
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.notif_config = {
+            'android': {'senderID': '575303166705'},
+            'ios': {'alert': 'true', 'badge': 'true', 'sound': 'true'},
+            'windows': {}
+        };
+
+        app.htreq = new plugin.HttpRequest();
+        app.push = PushNotification.init(app.notif_config);
+
+        // app.push.on('registration', function(data) {
+        //     app.push_token = data.registrationId;
+
+        //     $('.push_token').val(app.push_token);
+        //     app.send_push_token();
+        // });
+
+        $('.send_token').click(function() {
+            app.send_push_token();
+        });
+
+        $('.button-collapse').sideNav();
+
+        app.send_push_token = function() {
+            app.htreq.post(app.server_url + '/add-token', {
+                my_token: app.push_token
+            },function(err, data) {
+                alert(data);
+            });
+        };
+
+        app.push.on('notification', function(data) {
+            alert( JSON.stringify(data.additionalData) );
+        });
+
+        app.push.on('error', function(e) {
+            alert(e.message);
+        });
+    }
+};
+
+app.initialize();
